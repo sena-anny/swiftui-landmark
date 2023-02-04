95a3fac4c994ea9923e8ba7648ad43f9834ad878
sena-anny <cozykoji18@gmail.com>
2023-02-04 15:44:03 +0900

    Add navigation

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -9,9 +9,15 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        
-        List(landmarks) {landmark in
-            LandmarkRow(landmark: landmark)
+        NavigationView(){
+            List(landmarks) {landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
         }
 
     }
@@ -19,6 +25,10 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
     }
 }
