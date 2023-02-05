8e00fbecb9896cb96f147074faa5df1065ccdeef
sena-anny <cozykoji18@gmail.com>
2023-02-06 01:18:32 +0900

    add editMode

--- Landmarks/Views/Categories/CategoryHome.swift
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -9,15 +9,38 @@ import SwiftUI
 
 struct CategoryHome: View {
     @EnvironmentObject var modelData: ModelData
+    @State private var showingProfile = false
     
     var body: some View {
         NavigationView {
             List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height:200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                
                 ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
-                    Text(key)
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
                 }
+                .listRowInsets(EdgeInsets())
+                
             }
+            .listStyle(.inset)
                 .navigationTitle("Featured")
+                .toolbar {
+                    Button {
+                        showingProfile.toggle()
+                    }label: {
+                        Label("User Profile", systemImage: "person.crop.circle")
+                    }
+                }
+                .sheet(isPresented: $showingProfile) {
+                    ProfileHost()
+                        .environmentObject(modelData)
+                    
+                }
         }
     }
 }
